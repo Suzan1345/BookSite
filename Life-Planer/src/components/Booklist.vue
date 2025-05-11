501b9fbc9b917a6029bf6699c909c6346b763b6a
Suzan1345 <Suzan.Korkmaz@Student.HTW-Berlin.de>
2025-05-11 21:09:31 +0200

    M2

---
+++ Life-Planer/src/components/Booklist.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="(book, index) in books" :key="index">
+        {{ book.title }} von {{ book.author }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'BookList',
+  props: {
+    books: Array
+  }
+}
+</script>
